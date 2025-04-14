30d68c616b3ba3f3f43c37c489a06445f57f7b5b
Jonas Zakarias Lauritsen <jonas.lauritsendk@gmail.com>
2025-04-14 18:03:54 +0200

    Added the functions needed for the project

---
+++ main.py
@@ -0,0 +1,23 @@
+"""
+
+"""
+
+import numpy as np
+
+
+def heatCondCoeff(α: float, plateDisc: dict, dx: float, dy: float, dz: float):
+    pass
+
+def heatConvCoeff(h: float, area: float, plateDisc: dict, dx: float, dy: float, dz: float):
+    pass
+
+
+class CFPHE_plate:
+    """
+    
+    """
+    
+    def __init__(plate,
+                 startTempMatrix: np.ndarray,
+                 simTime: float):
+        pass
