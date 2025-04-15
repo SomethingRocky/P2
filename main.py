4dc16ff61e3df028d0e62364a75019fcacd3a457
Jonas Zakarias Lauritsen <jonas.lauritsendk@gmail.com>
2025-04-15 13:05:28 +0200

    Added some non complete, coeffient matrices

--- main.py
+++ main.py
@@ -4,9 +4,34 @@
 
 import numpy as np
 
+def xCondCoeff(coeff: float, matrixSize: int, plateDisc: dict, zConfines: tuple) -> np.ndarray:
+    
+    coeffMatrix = np.array((plateDisc["z"], plateDisc["y"], plateDisc["x"]))
+    
+    xCoeffVector = np.array((plateDisc["x"]))
+    
+    xCoeffVector[:3] = [coeff, -2*coeff, coeff]
+    #Rolling it back to match with the centered difference method
+    xCoeffVector = np.roll(xCoeffVector, -1)
+    for yIndex, yVec in enumerate(coeffMatrix[zConfines[0]: zConfines[1] + 1]): 
+        pass
+    
+def yCondCoeff(coeff: float, matrixSize: int, plateDisc: dict, zConfines: tuple) -> np.ndarray:
+    
+    coeffMatrix = np.array((plateDisc["z"], plateDisc["y"], plateDisc["x"]))
+    
+    
+    
+def zCondCoeff(coeff: float, matrixSize: int, plateDisc: dict, zConfines: tuple) -> np.ndarray:
+    
+    coeffMatrix = np.array((plateDisc["z"], plateDisc["y"], plateDisc["x"]))
+
+
 
 def heatCondCoeff(α: float, plateDisc: dict, dx: float, dy: float, dz: float):
-    pass
+    
+    matrixSize = plateDisc["x"] * plateDisc["y"] * plateDisc["z"]
+    
 
 def heatConvCoeff(h: float, area: float, plateDisc: dict, dx: float, dy: float, dz: float):
     pass
@@ -19,5 +44,6 @@ class CFPHE_plate:
     
     def __init__(plate,
                  startTempMatrix: np.ndarray,
-                 simTime: float):
+                 simTime: float,
+                 ):
         pass
